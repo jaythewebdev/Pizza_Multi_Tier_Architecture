58e4ba3a6e3fb02a66a032719643b7bbede800c8
jaythewebdev <jaythewebdev@gmail.com>
2023-07-12 03:36:48 +0530

    Pizza Multi Tier Initial

---
+++ PizzzaDALibrary/IRepo.cs
@@ -0,0 +1,18 @@
+﻿using PizzaModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PizzzaDALLibrary
+{
+    public interface IRepo
+    {
+        Pizza Get(int id);
+        Pizza Add(Pizza pizza, out bool status);
+        Pizza[] GetAll();
+        Pizza Update(Pizza pizza, out bool status);
+        Pizza Delete(int id, out bool status);
+    }
+}
